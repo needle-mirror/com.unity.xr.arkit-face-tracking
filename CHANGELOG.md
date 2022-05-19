3ed7d2c7e14e663e81c0e6f2a1303fd84ee276fd
Unity Technologies <@unity>
2022-05-19 00:00:00 +0000

    com.unity.xr.arkit-face-tracking@5.0.0-pre.12 ## [5.0.0-pre.12] - 2022-05-19
    ### Changed
    
    - Deprecation dialog will now be supressed in batch mode, as it cannot be clicked.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,12 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [5.0.0-pre.12] - 2022-05-19
+
+### Changed
+
+- Deprecation dialog will now be supressed in batch mode, as it cannot be clicked.
+
 ## [5.0.0-pre.9] - 2022-03-01
 
 No changes
--- Editor/DeprecratedWarning.cs
+++ Editor/DeprecratedWarning.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace UnityEditor.XR.ARKit
 {
     /// <summary>
@@ -12,6 +14,11 @@ namespace UnityEditor.XR.ARKit
         [InitializeOnLoadMethod]
         static void ShowWarning()
         {
+            if (Application.isBatchMode)
+            {
+                return;
+            }
+
             if (EditorUserSettings.GetConfigValue(k_HideWarningKey)?.Equals("true") ?? false)
             {
                 return;
